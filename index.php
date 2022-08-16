245f89ef4802ee5d1f04a8c74ee1cbbdc559e795
KarelProfant <Karel.Profant@seznam.cz>
2022-08-16 19:53:09 +0200

    Init homepage.

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="cs">
+	<head>
+		<title>Jack Jackson</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="/web/css/style.css">
+	</head>
+	<body>
+		<div id="layout">
+<?php
+include_once "web/php/header.php";
+?>
+			<section>
+				<article>
+				</article>
+			</section>
+<?php
+include_once "web/php/footer.php";
+?>
+		</div>
+	</body>
+</html>
