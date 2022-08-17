d90265df3a0830be5439a7fe45e7b8410430e121
KarelProfant <Karel.Profant@seznam.cz>
2022-08-17 20:53:37 +0200

    Add photo of people, header, article and button.

--- index.php
+++ index.php
@@ -14,8 +14,14 @@ include_once "web/php/header.php";
 ?>
 		<section id="section-main">
 			<section id="homepage-section-first">
-				<article>
+				<article id="article--homepage">
+					<h1>Jack Jackson</h1>
+					<p>What a bounder joseph stalin groomed sportacus testosterone trophy freestyle derek griffiths robot mustache, derek griffiths groomed testostherone trophy maequess of queensbury freestyle joseph stalin mustache Nostrills tickler sportacus what a bounder groomed?</p>
+					<form>
+						<button formaction="/learn-more">learn more</button>
+					</form>
 				</article>
+				<img src="/web/img/homepage/homepage--me.jpg" alt="me" id="img--homepage--me">
 			</section>
 		</section>
 <?php
